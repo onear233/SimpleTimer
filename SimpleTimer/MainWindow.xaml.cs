2cd221f879986d053ccb878e4954a51e4f0ce241
onear233 <world127617@outlook.com>
2025-08-03 18:48:59 +0800

    UI初步完成

--- SimpleTimer/MainWindow.xaml.cs
+++ SimpleTimer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +25,26 @@ namespace SimpleTimer
         {
             InitializeComponent();
         }
+
+
+
+        private void ButtonRestartApp_OnClick(object sender, RoutedEventArgs e)
+        {
+            ShowRestartDialog();
+        }
+
+        private async void ShowRestartDialog()
+        {
+            Application.Current.Shutdown();
+        }
+
+        
+
+        private void ButtonBaseToggleNavigationDrawer_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
     }
 }
