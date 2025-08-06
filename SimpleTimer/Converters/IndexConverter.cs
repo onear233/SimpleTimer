7c324fede0e6ccd851dc7b5a0720995e5e32ca94
onear233 <world127617@outlook.com>
2025-08-06 16:49:16 +0800

    创建配置文件UI+后台部分逻辑完成

---
+++ SimpleTimer/Converters/IndexConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SimpleTimer.Converters
+{
+    //索引转换器
+    public class IndexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int index)
+            {
+                return $"文本框 {index + 1}:";
+            }
+            return "文本框:";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SimpleTimer/ViewModels/ConfigWizardViewModel.cs
+++ SimpleTimer/ViewModels/ConfigWizardViewModel.cs
@@ -2,41 +2,103 @@
 using CommunityToolkit.Mvvm.Input;
 using SimpleTimer.Models;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Input;
 
 namespace SimpleTimer.ViewModels
 {
+    public partial class TextItem : ObservableObject
+    {
+        [ObservableProperty]
+        private string value;
+    }
+
     partial class ConfigWizardViewModel : ObservableObject
     {
+        //ObservableProperty
         [ObservableProperty]
         private int slideIndex = 0;
 
-        private ConfigModel configModel { get; set; }
+        [ObservableProperty]
+        private string configName = "DefaultName";
 
         [ObservableProperty]
-        private string configName;
+        private int timeIntervalCount = 1;
 
-        [ObservableProperty]
-        private int timeIntervalCount;
-
+        //Command
         public ICommand AddConfigCommand { get; set; }
-        
+
+
+        //存储第二页文本框内容的集合
+        public ObservableCollection<TextItem> TextValues { get; } = new ObservableCollection<TextItem>();
+
+        //存储第三页DataGrid内容的集合
+        [ObservableProperty]
+        private ObservableCollection<TimeList> timelineItems = new ObservableCollection<TimeList>();
+
+
+
+        public ConfigModel configModel = new ConfigModel();
 
         public ConfigWizardViewModel()
         {
-            AddConfigCommand = new RelayCommand(AddConfig, CanAddConfig);
+            AddConfigCommand = new RelayCommand(AddConfig);
         }
 
-        private bool CanAddConfig()
-        {
-            return true;
-        }
-
+        // 添加配置
         private void AddConfig()
         {
-            ConfigModel configModel = new ConfigModel();
             configModel.Name = ConfigName;
             configModel.TimeIntervalCount = TimeIntervalCount;
+            UpdateTextBoxes();
+            TimelineItems = GenerateData();
+        }
+
+        // 更新文本框集合
+        private void UpdateTextBoxes()
+        {
+            // 只在集合为空时初始化
+            if (TextValues.Count != TimeIntervalCount)
+            {
+                TextValues.Clear();
+                for (int i = 0; i < TimeIntervalCount; i++)
+                {
+                    TextValues.Add(new TextItem());
+                }
+            }
+        }
+
+        //获取所有文本框内容
+        public List<string> GetAllTextValues()
+        {
+            return TextValues.Select(tv => tv.Value).ToList();
+        }
+
+        // 获取特定索引的文本框内容
+        public string GetTextValueAt(int index)
+        {
+            if (index >= 0 && index < TextValues.Count)
+                return TextValues[index].Value;
+            return string.Empty;
+        }
+
+            
+        private ObservableCollection<TimeList> GenerateData()
+        {
+            ObservableCollection<TimeList> timeLists = new ObservableCollection<TimeList>();
+            for (int i = 0; i < configModel.TimeIntervalCount; i++)
+            {
+                timeLists.Add(new TimeList
+                {
+                    TimeIndex = i + 1,
+                    IsIncludePrevious = false,
+                    TimeName = GetTextValueAt(i),
+                    TimeLength = TimeSpan.FromMinutes(5 * (i + 1))
+                });
+            }
+            return timeLists;
         }
     }
 }
