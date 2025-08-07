8a0474a976c6108d99acb07ab06f506a86d2cbc4
onear233 <world127617@outlook.com>
2025-08-07 21:52:29 +0800

    去除不必要的引用

--- SimpleTimer/TimerWindow.xaml.cs
+++ SimpleTimer/TimerWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace SimpleTimer
 {
@@ -24,9 +13,10 @@ namespace SimpleTimer
             InitializeComponent();
         }
 
+        //窗口拖动
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            this.DragMove();
+            DragMove();
         }
     }
 }
