4abeada694b3a81ee5872edb1de3dc3a6fdfcbba
onear233 <world127617@outlook.com>
2025-09-04 20:13:08 +0800

    声音提醒完成

--- SimpleTimer/ViewModels/TimerViewModel.cs
+++ SimpleTimer/ViewModels/TimerViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.Toolkit.Uwp.Notifications;
 using SimpleTimer.Models;
 using System;
 using System.Windows;
@@ -12,7 +13,13 @@ namespace SimpleTimer.ViewModels
         [ObservableProperty]
         private TimeSpan remainingTime;
 
+        [ObservableProperty]
+        private string timeName;
+
         private DispatcherTimer timer;
+
+        // 在TimerViewModel中添加字段保存ConfigModel
+        private ConfigModel _lastConfig;
 
         public TimerViewModel()
         {
@@ -23,10 +30,13 @@ namespace SimpleTimer.ViewModels
             timer.Tick += Timer_Tick;
         }
 
-        // Implement the Receive method required by IRecipient<ConfigModel>
+        //Implement the Receive method required by IRecipient<ConfigModel>
         public void Receive(ConfigModel config)
         {
-            MessageBox.Show(config.TimeIntervalCount.ToString());
+            _lastConfig = config;
+            TimeName = config.TimeList[0].TimeName;
+            RemainingTime = config.TimeList[0].TimeLength;
+            StartCountdown();
         }
 
         public void StartCountdown()
@@ -45,11 +55,33 @@ namespace SimpleTimer.ViewModels
             if (RemainingTime > TimeSpan.Zero)
             {
                 RemainingTime -= TimeSpan.FromSeconds(1);
+                if (_lastConfig.TimeIntervalCount >= 1)
+                {
+                    for (int i = 1; i < _lastConfig.TimeIntervalCount; i++)
+                    {
+                        //检查是否需要切换为下一个内含时间段的名称
+                        if (_lastConfig.TimeList[i].IsIncludePrevious &&
+                        RemainingTime == _lastConfig.TimeList[i].TimeLength)
+                        {
+                            ringBell("当前时间段：" + _lastConfig.TimeList[i].TimeName);
+                            TimeName = _lastConfig.TimeList[i].TimeName;
+                        }
+                    }
+                }
             }
             else
             {
+                ringBell("计时结束");
                 timer.Stop();
             }
+        }
+        private void ringBell(string text)
+        {
+            new ToastContentBuilder()
+            .AddText("TimeIsLand")
+            .AddText(text)
+            .SetBackgroundActivation()
+            .Show();
         }
     }
 }
